PrzemoF/FreeCAD

FEM: add licence information to CalculiX inpwriter

src/Mod/Fem/ccxInpWriter.py
@@ -1,7 +1,36 @@
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2015 - Przemo Firszt <przemo@firszt.eu>                 *
+# *   Copyright (c) 2015 - Bernd Hahnebach <bernd@bimstatik.org>            *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+
 import FreeCAD
 import os
 import sys
 import time
+
+
+__title__ = "FemShellThickness"
+__author__ = "Przemo Firszt", "Bernd Hahnebach"
+__url__ = "http://www.freecadweb.org"
 
 
 class inp_writer:

kkremitzki/freecad-slimmed

src/Mod/Fem/ccxInpWriter.py
@@ -1,7 +1,36 @@
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2015 - Przemo Firszt <przemo@firszt.eu>                 *
+# *   Copyright (c) 2015 - Bernd Hahnebach <bernd@bimstatik.org>            *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+
 import FreeCAD
 import os
 import sys
 import time
+
+
+__title__ = "ccxInpWriter"
+__author__ = "Przemo Firszt, Bernd Hahnebach"
+__url__ = "http://www.freecadweb.org"
 
 
 class inp_writer: