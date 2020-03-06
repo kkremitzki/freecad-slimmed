kkremitzki/freecad-slimmed

commit 123fbcf54e95d0830e8087df9894b51111a4fbb0

FEM: use python base constraint class, mesh gmsh

src/Mod/Fem/femguiobjects/_ViewProviderFemMeshGmsh.py
@@ -31,6 +31,7 @@ import sys
 import FreeCAD
 import FreeCADGui
 import FemGui
+from . import ViewProviderFemConstraint
 
 # for the panel
 from femobjects import _FemMeshGmsh
@@ -41,38 +42,21 @@ from PySide.QtGui import QApplication
 import time
 
 
-class _ViewProviderFemMeshGmsh:
+class _ViewProviderFemMeshGmsh(ViewProviderFemConstraint.ViewProxy):
     "A View Provider for the FemMeshGmsh object"
-
-    def __init__(self, vobj):
-        vobj.Proxy = self
 
     def getIcon(self):
         return ":/icons/fem-femmesh-from-shape.svg"
 
-    def attach(self, vobj):
-        self.ViewObject = vobj
-        self.Object = vobj.Object
+    def setEdit(self, vobj, mode=0):
+        ViewProviderFemConstraint.ViewProxy.setEdit(
+            self,
+            vobj,
+            mode,
+            _TaskPanelFemMeshGmsh
+        )
 
-    def updateData(self, obj, prop):
-        return
-
-    def onChanged(self, vobj, prop):
-        return
-
-    def setEdit(self, vobj, mode):
-        # hide all meshes
-        for o in FreeCAD.ActiveDocument.Objects:
-            if o.isDerivedFrom("Fem::FemMeshObject"):
-                o.ViewObject.hide()
-        # show the mesh we like to edit
-        self.ViewObject.show()
-        # show task panel
-        taskd = _TaskPanelFemMeshGmsh(self.Object)
-        taskd.obj = vobj.Object
-        FreeCADGui.Control.showDialog(taskd)
-        return True
-
+    # overwrite unsetEdit, because the mesh should to be hided on task panel exit
     def unsetEdit(self, vobj, mode):
         FreeCADGui.Control.closeDialog()
         self.ViewObject.hide()  # hide the mesh after edit is finished
@@ -165,12 +149,6 @@ class _ViewProviderFemMeshGmsh:
             QMessageBox.critical(None, "Error in tree view", message)
             FreeCAD.Console.PrintError(message + "\n")
         return True
-
-    def __getstate__(self):
-        return None
-
-    def __setstate__(self, state):
-        return None
 
     def claimChildren(self):
         reg_childs = self.Object.MeshRegionList

src/Mod/Fem/femobjects/_FemMeshGmsh.py
@@ -27,10 +27,14 @@ __url__ = "http://www.freecadweb.org"
 #  \ingroup FEM
 #  \brief FreeCAD FEM _FemMeshGmsh
 
+from . import FemConstraint
 
-class _FemMeshGmsh():
+
+class _FemMeshGmsh(FemConstraint.Proxy):
     """A Fem::FemMeshObject python type, add Gmsh specific properties
     """
+
+    Type = "Fem::FemMeshGmsh"
 
     # they will be used from the task panel too, thus they need to be outside of the __init__
     known_element_dimensions = ["From Shape", "1D", "2D", "3D"]
@@ -55,9 +59,7 @@ class _FemMeshGmsh():
     ]
 
     def __init__(self, obj):
-        self.Type = "Fem::FemMeshGmsh"
-        self.Object = obj  # keep a ref to the DocObj for nonGui usage
-        obj.Proxy = self  # link between App::DocumentObject to this object
+        super(_FemMeshGmsh, self).__init__(obj)
 
         obj.addProperty(
             "App::PropertyLinkList",
@@ -206,13 +208,3 @@ class _FemMeshGmsh():
             "For each group create not only the elements but the nodes too."
         )
         obj.GroupsOfNodes = False
-
-    def execute(self, obj):
-        return
-
-    def __getstate__(self):
-        return self.Type
-
-    def __setstate__(self, state):
-        if state:
-            self.Type = state

commit e4f69b1b6b26a955dd8aea1d49afba94a46dadb3

FEM: gmsh mesh obj vp, another fix because of regression indroduced with 123fbcf5

src/Mod/Fem/femguiobjects/_ViewProviderFemMeshGmsh.py
@@ -53,6 +53,20 @@ class _ViewProviderFemMeshGmsh(ViewProviderFemConstraint.ViewProxy):
     def getIcon(self):
         return ":/icons/FEM_MeshGmshFromShape.svg"
 
+    def setEdit(self, vobj, mode):
+        # hide all meshes
+        for o in FreeCAD.ActiveDocument.Objects:
+            if o.isDerivedFrom("Fem::FemMeshObject"):
+                o.ViewObject.hide()
+        # show the mesh we like to edit
+        self.ViewObject.show()
+        # show task panel
+        taskd = _TaskPanel(self.Object)
+        # taskd.obj = vobj.Object
+        FreeCADGui.Control.showDialog(taskd)
+        return True
+
+    """
     def setEdit(self, vobj, mode=0):
         ViewProviderFemConstraint.ViewProxy.setEdit(
             self,
@@ -60,6 +74,7 @@ class _ViewProviderFemMeshGmsh(ViewProviderFemConstraint.ViewProxy):
             mode,
             _TaskPanel
         )
+    """
 
     # overwrite unsetEdit, hide mesh object on task panel exit
     def unsetEdit(self, vobj, mode):